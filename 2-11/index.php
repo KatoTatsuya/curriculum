6124807104ef596a914a978ca312ae56b86c3c9e
KatoTatsuya <68286073+KatoTatsuya@users.noreply.github.com>
2020-08-13 08:35:56 +0900

    PHP2章　フォームに入力した内容を取得する
    2-11ディレクトリの作成

---
+++ 2-11/index.php
@@ -0,0 +1,17 @@
+<form action="result.php" method="post">
+名前：<input type="text" name="my_name" />
+<br>
+Email：<input type="text" name="Email" />
+<br>
+パスワード：<input type="password" name="password" />
+<br>
+<input type="submit" value="送信" />
+</form>
+<br>
+<br>
+<p>1. 仕様書・設計書</p>
+<p>工業製品やソフトウェア、情報システム、サービスなどを開発する際に、備えるべき機能やその性能、特性、満たすべき要件などを図表や文章などで記述した文書。</p>
+<p>2. Git</p>
+<p>オープンソースの分散バージョン管理システムの一つ。複数の開発者が共同で一つのソフトウェアを開発する際などに、ソースコードやドキュメントなどの編集履歴を統一的に管理するのに用いられる。</p>
+<p>3. マージツール</p>
+<p>FDLファイル又はDTDファイルの変更内容をMDLファイルにマージできるツールのこと。</p>
