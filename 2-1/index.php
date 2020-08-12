8febd9513f06e4371b8795a31f6b7fa5ba5165e7
KatoTatsuya <68286073+KatoTatsuya@users.noreply.github.com>
2020-08-12 20:55:12 +0900

    Hello World
    2-1課題作成

---
+++ 2-1/index.php
@@ -0,0 +1,23 @@
+<?php
+// PHPの研修カリキュラム2-1
+?>
+<?php
+/*
+PHPの
+研修
+カリキュラム
+2-1
+*/
+?>
+<html>
+    <p>I say that <?php echo "Hello World!!"; ?></p>
+</html>
+
+<?php
+echo "1.開発環境・検証環境・本番環境"."<br>";
+echo "機器やソフトウェア、システムを開発するために必要な機材やソフトウェアなどの組み合わせ・装置やソフトウェア、システムの開発において、正しく動作するか検証する試験（テスト）のために用意された機材やソフトウェアなどの動作環境・情報システムやソフトウェアの開発で、できあがった製品を実際に稼働させて利用に供するための施設や機材、ソフトウェアなどを組み合わせた運用環境"."<br>";
+echo "2.基本設計書・詳細設計書"."<br>";
+echo "情報システム全体を機能単位に分割し、それぞれの機能がどういうものか、機能同士がつながるのか決めていく作業・何のためのシステムで、それを実現するためにどんな機能を持っているかを記載するもの"."<br>";
+echo "3.Ajax"."<br>";
+echo "ウェブブラウザ内で非同期通信を行いながらインターフェイスの構築を行うプログラミング手法"."<br>";
+?><img class="alignnone size-full wp-image-371" src="http://54.238.147.46/wp-content/uploads/2020/04/update_task_status.png" alt="" width="716" height="180" data-pagespeed-url-hash="608921261" onload="pagespeed.CriticalImages.checkImageForCriticality(this);">
