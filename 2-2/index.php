4e816d23491e6976cdd63ebfd3f832efe0cd9b41
KatoTatsuya <68286073+KatoTatsuya@users.noreply.github.com>
2020-08-12 20:56:24 +0900

    2-2課題作成
    PHP2章　変数とデータ型

---
+++ 2-2/index.php
@@ -0,0 +1,26 @@
+<?php
+$message = "Hello World!";
+var_dump($message);
+
+$x = 8;
+$y = 5.55;
+echo '<br>';
+var_dump($x);
+echo '<br>';
+var_dump($y);
+echo'<br>';
+$emp = null;
+var_dump($emp);
+echo '<br>';
+$en_words = 'Hello World';
+var_dump($en_words);
+echo '<br>';
+$ja_words = 'こんにちは';
+var_dump($ja_words);
+echo "<br>";
+echo "<br>";
+echo "1.API"."<br>";
+echo "アプリケーション・ソフトウェアを構築および統合するために使われるツール、定義、プロトコルのこと"."<br>";
+echo "2.SVN（Subversion）"."<br>";
+echo "著名なオープンソースのバージョン管理システムの一つ。複数の開発者が共同で一つのソフトウェアを開発する際などに、ソースコードやドキュメントを管理するのに用いられる。"."<br>";
+?>
